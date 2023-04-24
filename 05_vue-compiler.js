02b13ffc0d94082da524de768672bdb4a78cb580
b-hayes <bradozman@gmail.com>
2023-04-24 20:56:33 +1000

    notes

--- 05_vue-compiler.js
+++ 05_vue-compiler.js
@@ -1,7 +1,8 @@
-//this is an attempt as using Vue JS components without any framework.
+//this is an attempt at compiling vue components without a heavy framework.
 
 const compilerSfc = require("@vue/compiler-sfc")
 const { readFile, writeFile } = require('fs').promises
+const loader = require("vue-loader")
 
 readFile('./src/HelloWorld.vue', 'utf8').then(function (data) {
     var compiledTemplate = compilerSfc.compileTemplate({
@@ -9,15 +10,18 @@ readFile('./src/HelloWorld.vue', 'utf8').then(function (data) {
         source: data
     })
     console.log(compiledTemplate)
+    //This appears to work but still have no idea how to use the results in its current form.
+    //leaving this here in case there comes a day when it becomes worth figuring out.
 
-    var compiledScript = compilerSfc.compileScript(data, {
-        id: 'HelloWorld',
-        reactivityTransform: {}
-    })
-    console.log(compiledScript)
+    // var compiledScript = compilerSfc.compileScript(data, {
+    //     id: 'HelloWorld',
+    //     reactivityTransform: {}
+    // })
+    // console.log(compiledScript)
+
 
     //write the compiled content into a native Javascript ES6 Module
     // writeFile('./public/js/HelloWorld.js', compiled.content, 'utf8').then(function () {
-    //
+    //  //cant get this to work at all.
     // })
 })
