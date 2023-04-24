97c62dcb529cb90729af74e622453d948a8bc203
b-hayes <bradozman@gmail.com>
2023-04-24 21:26:35 +1000

    use the webpack bundle

---
+++ 06_webpack.js
@@ -0,0 +1,5 @@
+// configuring webpack
+// - npm install webpack webpack-cli --save-dev
+// - create an index.js
+// - add "build": "webpack" to scripts section of package.json
+// - run it with `npm run build`
