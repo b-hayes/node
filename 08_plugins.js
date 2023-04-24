fccefcfbec0872bc5e988c666a2f5a6888caabf4
b-hayes <bradozman@gmail.com>
2023-04-24 22:29:00 +1000

    plugins

---
+++ 08_plugins.js
@@ -0,0 +1,9 @@
+//Plugins can hook into the entire lifecycle of the build process
+// im not going to do plugins just yet but the basic idea is to
+// install one from https://webpack.js.org/plugins/
+//and then use them in the config like so..
+
+    // plugins: [
+    //     new BundleAnalyzerPlugin()
+    // ]
+
